4fa4c19c0ea721ce3ccf0c25f46e33e3efb904b4
Diego <diegoxavier.100@gmail.com>
2018-06-12 13:53:49 -0500

    Areas Peligrosas y Notificaciones de peligro

--- app/src/main/java/com/example/jona/latacungadigital/Activities/ARActivity.java
+++ app/src/main/java/com/example/jona/latacungadigital/Activities/ARActivity.java
@@ -2,66 +2,39 @@ package com.example.jona.latacungadigital.Activities;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
-import com.example.jona.latacungadigital.Activities.Permisos.LocationProvider;
 import com.example.jona.latacungadigital.R;
 import com.wikitude.architect.ArchitectStartupConfiguration;
 import com.wikitude.architect.ArchitectView;
 
-import java.io.IOException;
 
-public class ARActivity extends AppCompatActivity {
+public class ARActivity extends AppCompatActivity implements LocationListener {
 
     FloatingActionButton btn_salir ;
-    private ArchitectView architectView;
-    private LocationProvider locationProvider;
 
+    private ArchitectView architectView;
+
+    private LocationManager lm;
+
+    private double latitude;
+    private double longitude;
+    private double altitude = 0;
+    private float accuracy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
-
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED) {
-
-            SensorManager sensorManager =
-                    (SensorManager) getSystemService(SENSOR_SERVICE);
-
-            Sensor gyroscopeSensor =
-                    sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
-            SensorEventListener gyroscopeSensorListener = new SensorEventListener() {
-                @Override
-                public void onSensorChanged(SensorEvent sensorEvent) {
-                    // More code goes here
-                }
-
-                @Override
-                public void onAccuracyChanged(Sensor sensor, int i) {
-
-                }
-            };
-
-            sensorManager.registerListener(gyroscopeSensorListener,
-                    gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        }
-
-
-            btn_salir =  (FloatingActionButton) findViewById(R.id.fab);
+        btn_salir =  (FloatingActionButton) findViewById(R.id.fab);
 
         btn_salir.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,76 +43,117 @@ public class ARActivity extends AppCompatActivity {
             }
         });
 
-        architectView = (ArchitectView)this.findViewById( R.id.architectView );
+        this.architectView = (ArchitectView) this.findViewById(R.id.architectView);
         final ArchitectStartupConfiguration config = new ArchitectStartupConfiguration();
-        config.setFeatures(ArchitectStartupConfiguration.Features.Geo);
         config.setLicenseKey( "oWjZeBxLExDsXbbef5ylW6vZePQkJDE62FKnOp31hsz+EfEx6k4y8XJo3ir+3B9Vu3X8DAa60VH5gW/y5SjFsLZ4TWoHLcyLL/VFfmGtZNAPpN7jOk/xJ8rmTnVjE3W0iCbBJiD2WDqidCjSz5xRhWirT23wICtyPTCC05qyViRTYWx0ZWRfXwUhvhR3yQlpKY5VdWG6M+BLgaOMYQKgyMEMgJY51vVHJLaRW+KH9v7EhIONDB6xZXmIY87GCFPNhbPB5x8uHrm+beLEjBQvOHcwTg82m7mAg0TCWpw4Unh6VK8cLIruFHE1POkwGzUYevkYli1qYdjOi/wnpe/0S2RnAw36G5OQsVbgi/J7cxLoA8jGPr57IoKkBiP7nC+/BECQxtW8DryPFByZ6T/dyFK9Q0VBi2NGe2Ib4cOUC01Yt/kfqiDli5837Qn3Ii1pssMIbwK1zxHj0AO0ylfxiZ4l8pvb1RqieqilzSg2M7szwPSboI4WRLTqORGS9mPsW15A8MKpboAIuCI/g+oXAqtmuG0bw/iDvF6i5IevNDLRLsx22ipZtIqxZqQxxUOjmNo9YIjCpLp+UnhUHD2Zz9tzMFf9xwSuztwcLq89SCT3YZ55TvaycMztr9iouellYj6N/GB1C4231qvpMiPPZvf/sAaxpDueJP1QTjUkZEmbaAIRMOnoUMMciaRgxkOsJ+pVHFGwPnUvE9X7h0j7xQn8+BkDSsRVForVWPPd6MJahngefEyYR0wqpMUIVe5N" );
-
-        architectView.onCreate( config );
-
-        locationProvider = new LocationProvider(this, new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                if (location!=null && ARActivity.this.architectView != null ) {
-                    // check if location has altitude at certain accuracy level & call right architect method (the one with altitude information)
-                    if ( location.hasAltitude() && location.hasAccuracy() && location.getAccuracy()<7) {
-                        System.out.println("Location1: lat:"+location.getLatitude()+" , long:"+location.getLongitude()+" , altitud:"+location.getAltitude());
-                        ARActivity.this.architectView.setLocation( location.getLatitude(), location.getLongitude(), location.getAltitude(), location.getAccuracy() );
-                    } else {
-                        System.out.println("Location2: lat:"+location.getLatitude()+" , long:"+location.getLongitude()+" , altitud:"+location.getAltitude());
-                        ARActivity.this.architectView.setLocation( location.getLatitude(), location.getLongitude(), location.hasAccuracy() ? location.getAccuracy() : 1000 );
-                    }
-
-
-                }else{
-                    System.out.println("No entro !!");
-                }
-            }
-
-            @Override public void onStatusChanged(String s, int i, Bundle bundle) {}
-            @Override public void onProviderEnabled(String s) {}
-            @Override public void onProviderDisabled(String s) {}
-        });
-
-
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
+        this.architectView.onCreate(config);
     }
-
-
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+
         architectView.onPostCreate();
+
         try {
-            architectView.load(  "file:///android_asset/geo/index.html"  );
-        } catch (IOException e) {
-            e.printStackTrace();
+            this.architectView.load("file:///android_asset/realidad/index.html");
+            architectView.setLocation(latitude, longitude, altitude, accuracy);
+        } catch (Exception e) {
+            System.out.println("Error al cargar el asset de Wikitude");
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        architectView.onResume();
-        // start location updates
-        locationProvider.onResume();
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        architectView.onPause();
-        // stop location updates
-        locationProvider.onPause();
+        lm = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER))
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, this);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, this);
+
+        architectView.onResume();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
         architectView.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        lm.removeUpdates(this);
+
+        architectView.onPause();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //                               funciones de ubicación                                            //
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        altitude = 0;
+        accuracy = location.getAccuracy();
+        architectView.setLocation(latitude, longitude, altitude, accuracy); //actualización de posición
+
+    }
+
+    //Método que proporciona el estado de la función de ubicación (Disponible, temporalmente no disponible, fuera de servicio)
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        String newStatus = "";
+        switch (status) {
+            case LocationProvider.OUT_OF_SERVICE:
+                newStatus = "Fuera de servicio";
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                newStatus = "No Disponible";
+                break;
+            case LocationProvider.AVAILABLE:
+                newStatus = "Disponible";
+                break;
+        }
+        String msg = String.format((getResources().getString(R.string.provider_new_status)), provider, newStatus);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        //String msg = String.format(getResources().getString(R.string.provider_enabled, provider);
+        Toast.makeText(this, "Ubicación activada", Toast.LENGTH_SHORT).show();
+        architectView.setLocation(latitude, longitude, altitude, accuracy);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        //String msg = String.format(getResources().getString(R.string.provider_disabled), provider);
+        Toast.makeText(this, "Ubicación deshabilitada", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/jona/latacungadigital/EstructuraDatos/AreaPeligrosa.java
@@ -0,0 +1,57 @@
+package com.example.jona.latacungadigital.EstructuraDatos;
+
+public class AreaPeligrosa {
+    private String nombre, id;
+    private double radio, latitud, longitud;
+
+    public AreaPeligrosa() {
+    }
+
+    public AreaPeligrosa(String nombre, String id, double radio, double latitud, double longitud) {
+        this.nombre = nombre;
+        this.id = id;
+        this.radio = radio;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+}
