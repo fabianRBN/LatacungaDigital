833419a4fc2bcda5fd95448be925a64c8647ebbb
Francisco Rubio <francisrubio06@hotmail.com>
2018-07-04 21:20:55 -0500

    Correcion de error de Chatbot

--- app/src/main/java/com/example/jona/latacungadigital/Activities/RecycleMessageHolders/ChatBotMessageHolder.java
+++ app/src/main/java/com/example/jona/latacungadigital/Activities/RecycleMessageHolders/ChatBotMessageHolder.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.bumptech.glide.Glide;
+import com.example.jona.latacungadigital.Activities.ChatBotActivity;
 import com.example.jona.latacungadigital.Activities.Clases.CharacterClass;
 import com.example.jona.latacungadigital.Activities.modelos.TextMessageModel;
 import com.example.jona.latacungadigital.R;
@@ -30,11 +31,26 @@ public class ChatBotMessageHolder extends RecyclerView.ViewHolder {
         messageText.setText(message.getMessage()); // Se envia el mensaje del ChatBot.
         // Se coloca la imagen del personaje en el chatbot.
         CharacterClass characterClass = new CharacterClass();
-        characterClass.ReadCharacterFromDatabase(new CharacterClass.DataOfCharacters() {
-            @Override
-            public void dataCharacterSelected(String nameCharacter, String imageCharacterURL) {
-                Glide.with(context).load(imageCharacterURL).crossFade().centerCrop().into(circleImageView);
+            characterClass.ReadCharacterFromDatabase(new CharacterClass.DataOfCharacters() {
+                @Override
+                public void dataCharacterSelected(String nameCharacter, String imageCharacterURL) {
+                    if(isValidContextForGlide(context)) {
+                        Glide.with(context).load(imageCharacterURL).crossFade().centerCrop().into(circleImageView);
+                    }
+                }
+            });
+    }
+
+    public boolean isValidContextForGlide(final Context context) {
+        if (context == null) {
+            return false;
+        }
+        if (context instanceof ChatBotActivity) {
+            final ChatBotActivity activity = (ChatBotActivity) context;
+            if (activity.isDestroyed() || activity.isFinishing()) {
+                return false;
             }
-        });
+        }
+        return true;
     }
 }
